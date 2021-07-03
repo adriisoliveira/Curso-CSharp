8ab5167c2eaa32db0ffcf7276cb68198fddfd3ef
adriisoliveira <adrianne.soliveira@hotmail.com>
2021-07-03 17:53:54 -0300

    Modulo Arquivos

--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System;
+using System.Collections.Generic;
 
 namespace Arquivos
 {
@@ -7,38 +8,142 @@ namespace Arquivos
     {
         static void Main(string[] args)
         {
-            //string sourcePath = @"H:\temp\file 1.txt";
-            //string targetPath = @"H:\temp\file 2.txt";
+            string path = @"H:\temp\myFolder\file 1.txt";
+            string sourcePath = @"H:\temp\file 1.txt";
+            string targetPath = @"H:\temp\file 2.txt";
 
-            string path = @"H:\temp\file 1.txt";
-            FileStream fs = null;
-            StreamReader sr = null;
+            // CLASSE PATH
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
 
 
 
-            try
-            {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
 
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
-                //FileInfo fileInfo = new FileInfo(sourcePath);
-                ////fileInfo.CopyTo(targetPath);
-                //string[] lines = File.ReadAllLines(sourcePath);
-                //foreach(string line in lines)
-                //    Console.WriteLine(line);
 
-            }catch(IOException ex)
-            {
-                Console.WriteLine("An error ocurred");
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
-            }
+
+            ////DIRECTORY AND DIRECTORYINFO
+
+            ////Listar Diretorios
+            //try
+            //{
+            //    var folder =  Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+            //    Console.WriteLine("FOLDERS: ");
+            //    foreach(string s in folder)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.WriteLine("An ocurred a error: ", ex.Message);
+            //}
+
+            ////Listar os arquivos
+            //try
+            //{
+            //    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            //    Console.WriteLine("FILES: ");
+            //    foreach (string s in files)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.WriteLine("An ocurred a error: ", ex.Message);
+            //}
+
+            ////Criar uma pasta
+            //Directory.CreateDirectory(path + "\\NewFolder");
+
+
+            //STREAM WRITER
+            //try
+            //{
+            //    string[] lines = File.ReadAllLines(sourcePath);
+            //    using(StreamWriter sw = File.AppendText(targetPath))
+            //    {
+            //        foreach(string line in lines)
+            //        {
+            //            sw.WriteLine(line.ToUpper());
+            //        }                }
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.WriteLine("An ocurred a error: ", ex.Message);
+            //}
+
+
+            //try
+            //{
+            //    using (StreamReader sr = File.OpenText(path))
+            //    {
+            //        while (!sr.EndOfStream)
+            //        {
+            //            string line = sr.ReadLine();
+            //            Console.WriteLine(line);
+            //        }
+            //    }
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.WriteLine("An ocurred a error: ", ex.Message);
+            //}
+
+
+
+            //try
+            //{
+            //    using (FileStream fs = new FileStream(path, FileMode.Open))
+            //    {
+            //        using (StreamReader sr = new StreamReader(fs))
+            //        {
+            //            while (!sr.EndOfStream)
+            //            {
+            //                string line = sr.ReadLine();
+            //                Console.WriteLine(line);
+            //            }
+            //        }
+            //    }
+            //}catch(IOException ex)
+            //{
+            //    Console.WriteLine("An ocurred a error: ", ex.Message);
+            //}
+
+
+            //STREAM READER
+            //try
+            //{
+            //    fs = new FileStream(path, FileMode.Open);
+            //    sr = new StreamReader(fs);
+            //    //Ler o arquivo todo
+            //    while (!sr.EndOfStream)
+            //    {
+            //        string line = sr.ReadLine();
+            //        Console.WriteLine(line);
+            //    }
+            //    //FileInfo fileInfo = new FileInfo(sourcePath);
+            //    ////fileInfo.CopyTo(targetPath);
+            //    //string[] lines = File.ReadAllLines(sourcePath);
+            //    //foreach(string line in lines)
+            //    //    Console.WriteLine(line);
+
+            //}catch(IOException ex)
+            //{
+            //    Console.WriteLine("An error ocurred");
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    if (sr != null) sr.Close();
+            //    if (fs != null) fs.Close();
+            //}
         }
-    }
+        }
 }
---
+++ ArquivosExercicio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ArquivosExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourcePath = @"H:\temp\Exercicio\items.csv";
+            StreamReader sr = null;
+
+            var sumary = new StreamReader(File.OpenRead(sourcePath));
+            List<string> name = new List<string>();
+            List<string> value = new List<string>();
+            List<string> qnt = new List<string>();
+            while (!sumary.EndOfStream)
+            {
+                var line = sumary.ReadLine();
+                var values = line.Split(',');
+
+                name.Add(values[0]);
+                value.Add(values[1]);
+                qnt.Add(values[2]);
+
+
+
+            }
+
+        }
+    }
+}
